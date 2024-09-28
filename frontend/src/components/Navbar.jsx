1bfa589de3c2c867cfa0f9feee56a18eb4b2b179
Mxhran-m <mehranmulla367@gmail.com>
2024-09-29 00:51:19 +0530

    frontend added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+export default function Navbar() {
+  const list = ["Home", "About", "contact"];
+  const listItems = list.map((item, index) => {
+    return (
+      <a href="#" key={index} className="hover:underline cursor-pointer transition-all">
+        {item}
+      </a>
+    );
+  });
+  return (
+    <div className="flex justify-between w-full border-2 p-2 rounded-lg text-lg px-3 font-mono">
+      <div className="hover:underline cursor-pointer">Anon</div>
+      <div className="flex gap-3">{listItems}</div>
+    </div>
+  );
+}
